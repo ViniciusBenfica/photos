8857491b5395498ef122797252e3b242da8404ea
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-07-26 23:44:30 -0300

    CHORE continuing login creation with redux

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,27 @@
 import { NextPage } from "next";
 import Image from 'next/image'
+import { useEffect, useState } from "react";
 import Logo from "../../public/pin.png"
 import Header from "../components/header/header";
 
+interface user{
+    login: string,
+    password: string,
+}
+
 const Login: NextPage = () => {
+    const [user, setUser] = useState<user>({login: "", password: ""})
+
+    useEffect(() => {
+        console.log(user)
+    },[user])
+
     return(
         <div>
             <Header></Header>
             <div>Informe seu e-mail e senha</div>
-            <input placeholder="E-mail"></input>
-            <input placeholder="******"></input>
+            <input onChange={(e) => setUser((prevState) => ({...prevState, login: e.target.value}))} placeholder="E-mail"></input>
+            <input onChange={(e) => setUser((prevState) => ({...prevState, password: e.target.value}))} placeholder="******"></input>
             <button>Esqueci a senha</button>
             <button>Acessar Conta</button>
         </div>
